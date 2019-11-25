be15651ad1a7f2d3fd596c82976e84ab8d87b735
Phoenix <Phoenix@10.0.0.143>
2019-11-25 11:18:53 -0600

    first

---
+++ dndserver/src/main/java/com/gmail/phoenixjoe/dndserver/DndserverApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.phoenixjoe.dndserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndserverApplication.class, args);
+	}
+
+}
